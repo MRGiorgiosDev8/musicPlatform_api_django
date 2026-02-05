e0bb5e6c00c5bceedd3174135908c15305053866
Georgio <georgetrueman64@gmail.com>
2026-02-05 07:01:25 +0300

    style: обновлён внешний вид UI и добавлен анимированный гамбургер в navbar

---
+++ music_api/static/js/animation/hamburger_animation.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerTogglers = document.querySelectorAll('.hamburger.hamburger--elastic');
+
+    hamburgerTogglers.forEach(toggler => {
+        toggler.addEventListener('click', () => {
+            toggler.classList.toggle('is-active');
+        });
+    });
+});
+
